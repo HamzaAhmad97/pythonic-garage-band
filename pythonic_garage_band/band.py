c068a0ae75d1f0eaaf651042a773cf4dce9eeb7a
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-13 19:55:36 +0300

    added the guitarist class

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -54,3 +54,80 @@ class Musician(ABC):
         Abstract method -> Override
         """
         pass
+
+class Guitarist(Musician):
+    """
+    An class to represent a Guitarist.
+
+    Attributes
+    ----------
+    name : string
+        name of Guitarist
+    INSTRUMENT : string (class attribute)
+        guitar
+
+    Methods
+    -------
+    play_solo():
+        Returns a solo played by a Guitarist
+    get_instrument():
+        returns guitar
+    """
+    INSTRUMENT = 'guitar'
+
+    def __init__(self, name):
+        """
+        The constructor method for the class Guitarist
+
+        Parameters
+        ----------
+        name : string
+                The name of the Guitarist
+        """
+        self.name = name
+
+    def play_solo(self):
+        """
+        Return a solo played by the current Guitarist instance
+
+        Returns
+        -------
+        string
+            a solo played by a guitarist
+        """
+        return "face melting guitar solo"
+
+    def get_instrument(self):
+        """
+        Return guitar
+
+        Returns
+        -------
+        string
+            the instrument, which is the guitar
+        """
+        return Guitarist.INSTRUMENT
+
+    def __str__(self):
+        """
+        Return a string representation for a guitarist instance by str()
+
+        Returns
+        -------
+        string
+            Returns a string representation for a guitarist instance by str() print()
+
+        """
+        return f"My name is {self.name} and I play {Guitarist.INSTRUMENT}"
+
+    def __repr__(self):
+        """
+        Return a string representation for a guitarist instance by repr()
+
+        Returns
+        -------
+        string
+            Returns a string representation for a guitarist instance by str() repr()
+
+        """
+        return f"Guitarist instance. Name = {self.name}"
